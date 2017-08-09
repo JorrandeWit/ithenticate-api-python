c16a3a9604393ff1b0db0c1a951ed39eba5cd888
Jorran de Wit <jorrandewit@outlook.com>
2017-08-09 13:25:20 +0200

    Update methods

--- iThenticate/API/Treasure/documents.py
+++ iThenticate/API/Treasure/documents.py
@@ -8,7 +8,7 @@ class Document(object):
     def __init__(self, client):
         self.client = client
 
-    def add(self, file_path, folder_id, author_fist_name, author_last_name, title=None):
+    def add(self, file_path, folder_id, author_first_name, author_last_name, title=None):
         encoded = base64.b64encode(open(file_path, 'rb').read())
         filename = file_path.split('/')[-1]
         title = title or filename
@@ -20,9 +20,19 @@ class Document(object):
             author_last=author_last_name,
             base64=encoded,
             title=title,
-            author_first=author_fist_name,
+            author_first=author_first_name,
             folder_id=folder_id)
 
         xml_response = self.client.doHttpCall(data=xml_string)
 
         return List(xml_response, 'uploaded', self.client.getAPIStatus(xml_response), self.client.getAPIMessages(xml_response))
+
+    def all(self, folder_id):
+        xml_string = get_xml_as_string('get.xml')
+        xml_string = xml_string.format(sid=self.client._session_id,
+                                       method_name='folder.get',
+                                       id=folder_id)
+
+        xml_response = self.client.doHttpCall(data=xml_string)
+
+        return List(xml_response, 'documents', self.client.getAPIStatus(xml_response), self.client.getAPIMessages(xml_response))
--- iThenticate/API/Treasure/folders.py
+++ iThenticate/API/Treasure/folders.py
@@ -13,13 +13,3 @@ class Folder(object):
         xml_response = self.client.doHttpCall(data=xml_string)
 
         return List(xml_response, 'folders', self.client.getAPIStatus(xml_response), self.client.getAPIMessages(xml_response))
-
-    def get(self, folder_id):
-        xml_string = get_xml_as_string('get.xml')
-        xml_string = xml_string.format(sid=self.client._session_id,
-                                       method_name='folder.get',
-                                       id=folder_id)
-
-        xml_response = self.client.doHttpCall(data=xml_string)
-
-        return List(xml_response, 'documents', self.client.getAPIStatus(xml_response), self.client.getAPIMessages(xml_response))
