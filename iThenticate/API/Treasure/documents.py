78888d0ae96d42007ea74214896eeb85f8eeb957
Jorran de Wit <jorrandewit@outlook.com>
2017-08-12 15:46:02 +0200

    Add document.get() method

--- iThenticate/API/Treasure/documents.py
+++ iThenticate/API/Treasure/documents.py
@@ -1,7 +1,7 @@
 import base64
 
 from ..Helpers import get_xml_as_string
-from ..Object import List
+from ..Object import Data
 
 
 class Document(object):
@@ -9,6 +9,15 @@ class Document(object):
         self.client = client
 
     def add(self, file_path, folder_id, author_first_name, author_last_name, title=None):
+        """
+        Submit a new document to your iThenticate account.
+
+        :file_path: The path to the document on your machine
+        :folder_id: The folder where the document should be uploaded to
+        :author_first_name: First name of first author
+        :author_last_name: Last name of first author
+        :title: The title of the document to use in iThenticate
+        """
         encoded = base64.b64encode(open(file_path, 'rb').read())
         filename = file_path.split('/')[-1]
         title = title or filename
@@ -25,9 +34,16 @@ class Document(object):
 
         xml_response = self.client.doHttpCall(data=xml_string)
 
-        return List(xml_response, 'uploaded', self.client.getAPIStatus(xml_response), self.client.getAPIMessages(xml_response))
+        return Data(xml_response, 'uploaded',
+                    self.client.getAPIStatus(xml_response),
+                    self.client.getAPIMessages(xml_response))
 
     def all(self, folder_id):
+        """
+        Retrieve all documents within a folder
+
+        :folder_id: The folder_id to retrieve documents from.
+        """
         xml_string = get_xml_as_string('get.xml')
         xml_string = xml_string.format(sid=self.client._session_id,
                                        method_name='folder.get',
@@ -35,4 +51,23 @@ class Document(object):
 
         xml_response = self.client.doHttpCall(data=xml_string)
 
-        return List(xml_response, 'documents', self.client.getAPIStatus(xml_response), self.client.getAPIMessages(xml_response))
+        return Data(xml_response, 'documents',
+                    self.client.getAPIStatus(xml_response),
+                    self.client.getAPIMessages(xml_response))
+
+    def get(self, document_id):
+        """
+        Retrieve the current document status information within iThenticate.
+
+        :document_id: The document id as in iThenticate
+        """
+        xml_string = get_xml_as_string('get.xml')
+        xml_string = xml_string.format(sid=self.client._session_id,
+                                       method_name='document.get',
+                                       id=document_id)
+
+        xml_response = self.client.doHttpCall(data=xml_string)
+
+        return Data(xml_response, 'documents',
+                    self.client.getAPIStatus(xml_response),
+                    self.client.getAPIMessages(xml_response))
