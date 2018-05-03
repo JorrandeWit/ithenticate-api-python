3f7b6e1dc7c4253a7d75e41a5f04ba809b0f7131
Jorran de Wit <jorrandewit@outlook.com>
2018-05-03 16:37:30 +0200

    Fix: all() methods did not return all list items.

--- iThenticate/API/Object/data.py
+++ iThenticate/API/Object/data.py
@@ -1,8 +1,6 @@
 class Data(dict):
     def __init__(self, xml, status=None, messages=None):
-        """
-        Process the freakin' xml instance to a friendly dictionary!
-        """
+        """Process the xml instance into a friendly dictionary."""
 
         content = {
             'data': None,
@@ -20,38 +18,32 @@ class Data(dict):
         # All properties in a single item
         _data = {}
         for node in nodes:
-            value_node = node.find('value')
+            if node.tag == 'member':
+                # Dictionary item
+                key = node.find('name').text
+                value_node = node.find('value')[0]
 
-            if not value_node:
+                if value_node.tag == 'int':
+                    value = int(value_node.text.strip())
+                elif value_node.tag in ['array', 'struct', 'data', 'param']:
+                    value = self._breakdown_tree(value_node.findall('./'))
+                elif value_node.tag == 'string':
+                    try:
+                        value = value_node.text.strip()
+                    except AttributeError:
+                        # Maliciously constructed data is detected in the responses for the string nodes
+                        value = value_node.text
+                else:
+                    # dateTime.iso8601 or something exotic
+                    value = value_node.text
+
+                _data[key] = value
+            elif node.tag == 'value':
+                # Nodes are list items
+                if not isinstance(_data, list):
+                    _data = []
+                _data.append(self._breakdown_tree(node.findall('./')))
+            else:
                 # Recursively find data as this is not a data node
                 return self._breakdown_tree(node.findall('./'))
-            else:
-                key = node.find('name')
-                value_node = value_node[0]
-
-            # Value of node
-            if value_node.tag == 'int':
-                value = int(value_node.text.strip())
-            elif value_node.tag in ['array', 'struct', 'data', 'param']:
-                value = self._breakdown_tree(value_node.findall('./'))
-            # elif value_node.tag == 'struct':
-            #     value = self.break_down_struct_node(value_node)
-            elif value_node.tag == 'string':
-                try:
-                    value = value_node.text.strip()
-                except AttributeError:
-                    # Maliciously constructed data is detected in the responses for the string nodes
-                    value = value_node.text
-            else:
-                # dateTime.iso8601 or something exotic
-                value = value_node.text
-
-            try:
-                if key.text in ['api_status', 'status', 'messages', 'sid']:
-                    # Exclude this, it'll just be duplicate in the response dictionary
-                    continue
-                _data[key.text] = value
-            except AttributeError:
-                # Root element is list as its node won't have a name
-                _data = [value]
         return _data
